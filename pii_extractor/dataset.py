342072b6082f64cae9960033978c2d81b4fc9cc0
Phish <69989217+phishontop@users.noreply.github.com>
2023-02-18 17:11:25 +0000

    commit

---
+++ pii_extractor/dataset.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+from typing import List
+from collections import defaultdict
+import json
+
+
+@dataclass
+class DatasetItem:
+    name: str
+    keywords: List[str]
+    type: str
+
+
+class Dataset:
+
+    def __init__(self):
+        with open("pii_dataset.json") as f:
+            self.dataset = json.load(f)
+
+        self.information = defaultdict(list)
+        self._dataset_items = None
+
+    @property
+    def dataset_items(self) -> List[DatasetItem]:
+        if self._dataset_items is None:
+            self._dataset_items = [
+                DatasetItem(name=name, **data)
+                for name, data in self.dataset.items()
+                if data["type"] != "dont_add_this"
+            ]
+
+        return self._dataset_items
+
+    def check_words(self, words: List[str]):
+        check_functions = {
+            "contains": self.contains_keywords,
+            "list": self.contains_list
+        }
+
+        for dataset_item in self.dataset_items:
+            function = check_functions[dataset_item.type]
+            function(words=words, item=dataset_item)
+
+    def contains_keywords(self, words: List[str], item: DatasetItem):
+        for word in words:
+            if word in item.keywords:
+                self.information[item.name].append(word)
+
+    @staticmethod
+    def does_contain_multiple(words: List[str], current_count: int, item: DatasetItem) -> bool:
+        for keyword in item.keywords:
+            keyword_words = keyword.split()
+            iter_count = current_count
+            has_broke = False
+            for keyword_word in keyword_words:
+                if keyword_word not in words[iter_count]:
+                    has_broke = True
+                    break
+
+                iter_count += 1
+            if not has_broke:
+                return True
+
+        return False
+
+    def contains_list(self, words: List[str], item: DatasetItem) -> None:
+        collection = {"enabled": False, "collected": [], "stop_index": 0}
+        for count, word in enumerate(words):
+            if word in self.dataset["connectors"]["keywords"]:
+                collection["stop_index"] = count + 2
+
+            elif collection["stop_index"] == count and collection["stop_index"] != 0:
+                collection["enabled"] = False
+
+            elif self.does_contain_multiple(words=words, current_count=count, item=item):
+                collection["enabled"] = True
+
+            elif collection["enabled"]:
+                collection["collected"].append(word)
+
+        if collection["collected"]:
+            self.information[item.name] += collection["collected"]
---
+++ pii_extractor/extractor.py
@@ -0,0 +1,17 @@
+from .dataset import Dataset
+from typing import Dict, List
+
+
+class Extractor(Dataset):
+
+    def __init__(self, words: list) -> None:
+        super().__init__()
+        self.words = words
+
+    @classmethod
+    def from_text(cls, text_words: str):
+        return cls(words=text_words.split())
+
+    def run(self) -> Dict[str, List[str]]:
+        self.check_words(words=self.words)
+        return dict(self.information)
