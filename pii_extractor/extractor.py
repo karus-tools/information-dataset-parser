91f8ba5d1b987d4dc5f98039a2288f54c6196934
Phish <69989217+phishontop@users.noreply.github.com>
2023-02-20 15:41:14 +0000

    fixed a typing :)

--- pii_extractor/extractor.py
+++ pii_extractor/extractor.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 class Extractor(Dataset):
 
-    def __init__(self, words: list) -> None:
+    def __init__(self, words: List[str]) -> None:
         super().__init__()
         self.words = words
 
